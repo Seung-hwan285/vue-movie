e21aca1230ffc87e1c269743b00647aa12d81551
Seung-hwan285 <ghks123285@gmail.com>
2022-12-05 23:37:00 +0900

    feat :eee

--- test/api/index.js
+++ test/api/index.js
@@ -3,18 +3,12 @@ import fetch from 'node-fetch';
 
 export default async function handler(request, response) {
 
-    const {body} =request;
+    const {method, body} =request;
 
-    const res = await fetch(`/api/getMovie`,{
-        method:'POST',
-        body : JSON.stringify(request)
-    });
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&s=${body.title2}`);
     const data = await res.json();
     return response.status(200).json({ data });
 }
-
-// http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&s=${body.title2}
-
 // &y=${this.year}
 // export default async function handler(request, response) {
 //
