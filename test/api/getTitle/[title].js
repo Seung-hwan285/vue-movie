c806943c95980cd428468d24392b5e12537e493f
Seung-hwan285 <ghks123285@gmail.com>
2022-12-07 02:52:57 +0900

    feat : process

--- test/api/getTitle/[title].js
+++ test/api/getTitle/[title].js
@@ -2,9 +2,9 @@ import fetch from "node-fetch";
 
 export default async function handler(request, response) {
 
-    const {title,year} =request.query.body;
+    const {title} =request.query;
 
-    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&s=${title}&y=${year}`);
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&s=${title}`);
     const data = await res.json();
     return response.status(200).json({ data });
 }
