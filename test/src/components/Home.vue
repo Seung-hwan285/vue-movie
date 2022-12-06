61191bfe17e5ac67b722133b8893c61d7a6bb8be
Seung-hwan285 <ghks123285@gmail.com>
2022-12-07 02:12:45 +0900

    test

--- test/src/components/Home.vue
+++ test/src/components/Home.vue
@@ -79,12 +79,7 @@ export default {
 
         console.log(year);
         // body 안넘어오고 있음 이거 해결해야함
-        const response = await fetch(`/api/get`,{
-          body:JSON.stringify({
-            title :title,
-            year : year,
-          })
-        });
+        const response = await fetch(`/api/get/${title}/${year}`);
 
         console.log(response);
         const result =await response.json();
