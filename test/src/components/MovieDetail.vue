25eb10960ae72662ff13f3fc1f58836f280adb40
Seung-hwan285 <ghks123285@gmail.com>
2022-12-07 03:23:09 +0900

    feat : process

--- test/src/components/MovieDetail.vue
+++ test/src/components/MovieDetail.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="movie-detail">
+  <div class="movie-detail" v-if="isShow">
+
+
 
       <h2>{{movie.Title}}</h2>
       <img :src="movie.Poster"/>
       <p>{{movie.Plot}}</p>
-
   </div>
+
+  <div class="container" v-else>
+    <div class="spinner"><i class="fas fa-spinner fa-10x"></i></div>
+  </div>
+
 </template>
 
 <script>
@@ -15,16 +21,29 @@ import {onMounted, ref} from "vue";
 
 export default {
 
+  data(){
+    return{
+      isShow : false,
+    }
+  },
+
   name: "MovieDetail",
-  setup(){
-    const route= useRoute();
+  setup() {
+    const route = useRoute();
     const movie = ref({});
 
-    onMounted(async ()=>{
+    onMounted(async () => {
       const id = route.params.id;
       const response = await fetch(`/api/getId/${id}`);
-      const result =await response.json();
-      movie.value =result.data;
+      const result = await response.json();
+      movie.value = result.data;
+
+
+      this.isShow=false;
+
+      setTimeout(()=>{
+        this.isShow=true;
+      },4000);
       // fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&i=${route.params.id}&plot=full`)
       // .then((res)=>res.json())
       // .then((data)=>{
@@ -66,6 +85,29 @@ p {
   color: black;
   font-size: 18px;
   line-height: 1.4;
+}
+
+.container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container{
+  color: #FFFFFF;
+}
+
+.spinner {
+  animation: spin 2s linear infinite;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 
 
