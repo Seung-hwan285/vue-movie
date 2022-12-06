75b241aa14255c56dd42fabf11faf6e1560531dd
Seung-hwan285 <ghks123285@gmail.com>
2022-12-07 00:02:43 +0900

    feat : process

--- test/src/components/Home.vue
+++ test/src/components/Home.vue
@@ -79,7 +79,13 @@ export default {
 
         console.log(year);
         // body 안넘어오고 있음 이거 해결해야함
-        const response = await fetch(`/api/get/${title}/${year}`);
+        const response = await fetch(`/api/get/`,{
+          method:'POST',
+          body:{
+            title : title,
+            year :year,
+          }
+        });
         const result =await response.json();
 
         this.movies=result.data.Search;
