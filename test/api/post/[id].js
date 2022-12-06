0bfc44b2af2157228e5ec22a0466fb999c8060df
Seung-hwan285 <ghks123285@gmail.com>
2022-12-06 21:45:31 +0900

    Feat :test

--- test/api/post/[id].js
+++ test/api/post/[id].js
@@ -6,6 +6,9 @@ export default async function handler(request, response) {
     const {id} =request.query;
 //  fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&i=${route.params.id}&plot=full`)
 
+
+
+    
     const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&i=${id}&plot=full`);
     const data = await res.json();
     return response.status(200).json({ data });
