e9100aed9d3fc03d2e36032549c1f83444c92bd3
Seung-hwan285 <ghks123285@gmail.com>
2022-12-06 14:53:45 +0900

    feat: tes

--- test/api/get/[title,year].js
+++ test/api/get/[title,year].js
@@ -5,7 +5,7 @@ export default async function handler(request, response) {
 
     const {title,year} =request.query;
 
-    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&s=${title}&y={year}`);
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&s=${title}&y=${year}`);
     const data = await res.json();
     return response.status(200).json({ data });
 }
