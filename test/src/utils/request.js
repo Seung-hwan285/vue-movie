0ead8b14e6d11406a422e14ad153f81edf0e5986
Seung-hwan285 <ghks123285@gmail.com>
2022-12-08 02:33:43 +0900

    feat : vuex 쓰기전

--- test/src/utils/request.js
+++ test/src/utils/request.js
@@ -3,17 +3,20 @@ import fetch from "node-fetch";
 
 const api={
     GET_TITLE_YEAR :'/api/get',
-    GET_TITLE: '/api/getTitle',
+    GET_TITLE: '/api/getTitle/',
     GET_ID:'/api/id'
 }
 
 export const movieAPI={
+
+
+
     getTitleAndYear  : async (title,year)=>{
         const response = await fetch(`${api.GET_TITLE_YEAR}/${title}/${year}`);
         return  response.json();
     },
     getTitle : async (title)=>{
-        const response =await fetch(`${api.GET_TITLE}/${title}`);
+        const response =await fetch(`${api.GET_TITLE}${title}`);
         return response.json();
     },
     getId : async (id)=>{
