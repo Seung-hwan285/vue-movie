9557cb16d50713c07e3e6f87e0f15806fb0330ec
Seung-hwan285 <ghks123285@gmail.com>
2022-12-08 02:15:48 +0900

    feat : vuex 쓰기전

--- test/api/getTitle/[title].js
+++ test/api/getTitle/[title].js
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
-const {API_END_POINT,VUE_APP_API_KEY} =require('/test/src/utils/constant.js');
 
 export default async function handler(request, response) {
 
     const {title} =request.query;
 
-    const res = await fetch(`${API_END_POINT}?apikey=${process.env.VUE_APP_API_KEY}&s=${title}`);
+    //.왜 안될까~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&s=${title}`);
     // const res = await fetch(`${API_END_POINT}?apikey=${VUE_APP_API_KEY}&s=${title}`);
     const data = await res.json();
     return response.status(200).json({ data });
