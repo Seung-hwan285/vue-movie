f8c2db6e2e96aea1b225dbfdc77b3f564904185a
Seung-hwan285 <ghks123285@gmail.com>
2022-12-08 02:51:17 +0900

    feat : vuex 쓰기전

--- test/api/getId/[id].js
+++ test/api/getId/[id].js
@@ -2,11 +2,9 @@ import fetch from 'node-fetch';
 
 
 export default async function handler(request, response) {
-    const API_END_POINT = 'http://www.omdbapi.com/'
-    const VUE_APP_API_KEY=process.env;
-    const {id} =request.query;
 
-    const res = await fetch(`${API_END_POINT}?apikey=${VUE_APP_API_KEY}&i=${id}&plot=full`);
+    const {id} =request.query;
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.VUE_APP_API_KEY}&i=${id}&plot=full`);
     const data = await res.json();
     return response.status(200).json({ data });
 }
