db0aeab22c4d97af6739783c1c80e665658b94e3
Seung-hwan285 <ghks123285@gmail.com>
2022-12-08 02:19:46 +0900

    feat : vuex 쓰기전

--- test/api/getTitle/[title].js
+++ test/api/getTitle/[title].js
@@ -8,8 +8,9 @@ export default async function handler(request, response) {
 
     const {title} =request.query;
 
-
-    const res = await fetch(`${API_END_POINT}?apikey=${VUE_APP_API_KEY}&s=${title}`);
+    //.왜 안될까~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    const res = await fetch(`${API_END_POINT}?apikey=${process.env.VUE_APP_API_KEY}&s=${title}`);
     // const res = await fetch(`${API_END_POINT}?apikey=${VUE_APP_API_KEY}&s=${title}`);
     const data = await res.json();
     return response.status(200).json({ data });
